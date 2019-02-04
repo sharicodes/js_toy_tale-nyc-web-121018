476106e4c14ad67879fa7c44556c5e8e0f3b5971
sharicodes <srosenb@yahoo.com>
2019-02-04 14:16:44 -0500

    finishedgit add .git add .

--- src/index.js
+++ src/index.js
@@ -4,101 +4,109 @@ const toyForm = document.querySelector('.container')
 let addToy = false
 
 // YOUR CODE HERE
+//added for making sure page loaded
+document.addEventListener("DOMContentLoaded", function (event){
+//add constanst for use in eventslisteners, etc
+const toyContainer = document.getElementById('toy-collection')
+const toysURL = "http://localhost:3000/toys"
+const newToyForm = document.querySelector(".add-toy-form")
+//to hold all of the toys
 let allToys = []
 
-//given
+//given for add new toy button
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
-  // submit listener here
+    // submit listener here
   } else {
-  toyForm.style.display = 'none'
+    toyForm.style.display = 'none'
   }
 })
-//OR HERE!
-document.addEventListener("DOMContentLoaded", function(event) {
-  //set variables from DOM
-const toyCollection = document.getElementById("toy-collection")
-const newToyForm = document.querySelector(".add-toy-form")
-const allToyCards = document.querySelectorAll(".card")
 
 
-//fetch toys and show on page- step 2
-function fetchToys() {
-fetch('http://localhost:3000/toys')
-.then(response => response.json())
-.then( data => {
-allToys = data
-//call showAllToys helper
-showAllToys(allToys)
-})
-}
-fetchToys()//call fetchToys helper
+// OR HERE!
+//fetch to add the toys to page
+fetch(`${toysURL}`)
+.then( response => response.json())
+.then(  toyData => toyData.forEach(function(toy){
+  allToys = toyData
+  toyContainer.innerHTML +=`
+  <div class="card">
+    <h2>${toy["name"]}</h2>
+    <img src="${toy["image"]}"class="toy-avatar" />
+    <p>${toy["likes"]} Likes</p>
+    <button class="like-btn" data-id ="${toy["id"]}" >Like </button>
+  </div> `
+}))//end of for each
 
-
-//showAllToys helper function itterates through the HTML of all the toys abd maps that to a new toy
-function showAllToys(toys) {
-toyCollection.innerHTML = toys.map(makeNew).join("")
-}
-
-newToyForm.addEventListener('submit', e => {
-e.preventDefault()
+//add new toy to DOM and Db
+newToyForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  //console.log(e.target)
 const name = e.target.name.value
 const image = e.target.image.value
+fetch(`${toysURL}`, {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: name,
+    image: image,
+    likes: 0
+  })
+})//end of fetch
+.then( response => response.json())
+.then(  toy => {
+    toyContainer.innerHTML += `
+    <div class="card">
+      <h2>${toy["name"]}</h2>
+      <img src="${toy["image"]}"class="toy-avatar" />
+      <p>${toy["likes"]}</p>
+      <button class="like-btn" data-id ="${toy["id"]}" >Like </button>
+    </div> `
+  })
+})//end of newToyForm event listener
 
-fetch("http://localhost:3000/toys", {
-method: "post",
-headers: {
-"Content-Type": "application/json",
-"Accept": "application/json"
-},
-body: JSON.stringify({
-name: name,
-image: image,
-likes: 0
-})
-}).then(response => response.json())
-.then(toy => {
-allToys.push(toy)
-//console.log(toy);
-toyCollection.innerHTML += makeNew(toy)
-})
-})
+//update the likes by clicking on button
+toyContainer.addEventListener('click', (e) => {
+  console.log(e.target) //works and gives id
+let currentLikes = parseInt(e.target.previousElementSibling.innerText)
+// console.log(currentLikes);
+let newLikes = currentLikes + 1;
+// console.log(newLikes)
+e.target.previousElementSibling.innerText = newLikes + " Likes"
 
-toyCollection.addEventListener('click', e => {
-//1. find in array
-const toyObject = allToys.find(toy => toy.id === parseInt(e.target.dataset.id))
-const indexOfToy = allToys.indexOf(toyObject)
-let currentLikes = toyObject["likes"]
-currentLikes++
-//console.log(currentLikes);
-//console.log(indexOfToy);
+// when i click the likes should ++
 
-fetch(`http://localhost:3000/toys/${e.target.dataset.id}`, {
-method: 'PATCH',
-headers: {
-"Content-Type": "application/json",
-"Accept": "application/json"
-},
-body: JSON.stringify({
-likes: currentLikes
-})
-})
+// from e.target get to "likes"
+// we can use previousSibling()
+// FOR THIS EXAMPLE ONLY BECAUSE HTML will be different every time
+// Find the relation to e.target || use another querySelector
 
-allToys[indexOfToy]['likes'] = currentLikes
-showAllToys(allToys)
-})
+//we need to update the DOM and Update the database
 
-}); // end of DOM Content Loaded
+//how do we pull the exact button for the right card
+//how do we update the likes on that particular card?
 
+  fetch(`${toysURL}/${e.target.dataset.id}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      likes: newLikes
+    //})
+  })//end of fetch
+  // .then( response => response.json())
+  // .then(  toy => {
+  //     // updateDOM
+    })
 
-function makeNew(toy) {
-return `<div class="card">
-<h2>${toy["name"]}</h2>
-<img src=${toy["image"]} class="toy-avatar" />
-<p>${toy["likes"]} Likes </p>
-<button class="like-btn" data-id="${toy["id"]}">Like </button>
-</div>`
-}
+})//end toy container listener
+
+}) //end of DOMContentLoaded
