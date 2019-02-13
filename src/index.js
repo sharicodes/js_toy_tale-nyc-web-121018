d4fd2b43a412483512ff6f1656cdabffca85930e
sharicodes <srosenb@yahoo.com>
2019-02-13 07:54:12 -0500

    45min

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+const toyCollection = document.querySelector("#toy-collection")
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
 // YOUR CODE HERE
-document.addEventListener("DOMContentLoaded", ()=>{
-const toyContainer = document.querySelector("#toy-collection")
-const newToyForm = document.querySelector(".add-toy-form")
+
 
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
@@ -18,101 +19,84 @@ const newToyForm = document.querySelector(".add-toy-form")
     }
   })//end add Btn given
   // OR HERE!
-
-  fetch("http://localhost:3000/toys")
+fetch("http://localhost:3000/toys")
   .then(r => r.json())
-  .then(toys => {
-    let toyHTML = toys.map(toy =>{
+  .then(toyData =>{
+    let toyHTML = toyData.map(toy =>{
       return `
-      <div class ="card">
+      <div class="card">
       <h2>${toy.name}</h2>
-      <img src=${toy.image} class="toy-avatar"/>
-      <p>${toy.likes} Likes</p>
-      <button data-id=${toy.id} class="like-btn">Like <3</button>
-      <button data-id=${toy.id} class="delete-btn">Delete</button>
-      </div>
-      `
-    })//end of map
-    toyContainer.innerHTML += toyHTML.join('')
-  })//end of fetch
+      <img src=${toy.image} class="toy-avatar" />
+      <p data-id=${toy.id}>${toy.likes} Likes </p>
+      <button class="like-btn" data-id=${toy.id}>Like <3</button>
+      </div>`
+    })//end map
+    toyCollection.innerHTML = toyHTML.join('')
+  })//end then
 
-//add a new toys
-toyForm.addEventListener("submit", (e)=> {
+toyForm.addEventListener("submit",(e)=>{
   e.preventDefault()
   //console.log(e.target);
   const newToyName = e.target.name.value
   const newToyImage = e.target.image.value
   //console.log(newToyName, newToyImage);
-  fetch("http://localhost:3000/toys",{
+  fetch("http://localhost:3000/toys", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-       "Accept": "application/json"
-     },
-     body: JSON.stringify({
-       name: newToyName,
-       image: newToyImage,
-       likes: 0
-    })
-  })//end of fetch
-  .then(r => r.json())
-  .then(newToy => {
-    let newToyHTML =  `
-      <div class ="card">
-      <h2>${newToy.name}</h2>
-      <img src=${newToy.image} class="toy-avatar"/>
-      <p>Likes: ${newToy.likes}</p>
-      <button data-id="${newToy.id}"class="like-btn">Like <3</button>
-      <button data-id=${toy.id} class="delete-btn">Delete</button>
-      </div>
-      `
-    toyContainer.innerHTML += newToyHTML
-    e.target.reset()
-  })//end of then
-})//end of toyform event listener
-
-//increase Likes
-//add event addEventListener
-toyContainer.addEventListener("click", (e)=>{
-  if(e.target.className === "like-btn"){
-      //console.log(e.target)
-  //identify likes container
-  let currLikes = parseInt(e.target.previousElementSibling.innerText)
-  let newLikes= currLikes+1
-  //console.log(newLikes);
-  e.target.previousElementSibling.innerText = newLikes + " likes"
-
-//console.log(e.target.dataset.id);
-  fetch(`http://localhost:3000/toys/${e.target.dataset.id}`,{
-    method: "PATCH",
-    headers: {
+    headers:{
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
     body: JSON.stringify({
-      likes: newLikes
-
+      name: newToyName,
+      image: newToyImage,
+      likes: 0
     })
-
   })//end fetch
+  .then(r => r.json())//forgot
+  .then(newToy => {//forgot
+    let newToyHTML = `
+    <div class="card">
+    <h2>${newToy.name}</h2>
+    <img src=${newToy.image} class="toy-avatar" />
+    <p data-id=${newToy.id}>${newToy.likes} Likes </p>
+    <button class="like-btn" data-id=${newToy.id}>Like <3</button>
+    </div>
+    `
+    toyCollection.innerHTML += newToyHTML
+
+})
+
+})//end of addEventListener
+
+toyCollection.addEventListener("click", (e) =>{
+//console.log(e.target);
+  if(e.target.className === "like-btn"){
+  //console.log(e.target.className);
+    let likesNum = parseInt(e.target.previousElementSibling.innerText)
+    //console.log(likesNum)
+    let newLikesNum = likesNum+1
+    e.target.previousElementSibling.innerText = `${newLikesNum} Likes`
+    let currToyId = e.target.dataset.id
+    console.log(currToyId);
+
+    fetch(`http://localhost:3000/toys/${currToyId}`, {
+      method: "PATCH",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        likes: newLikesNum
+      })
+    })//end fetch
+
+  }//end if
+
+})//end of addEventListener
 
 
-  }//end of if
-  if(e.target.className === "delete-btn"){
-      //console.log(e.target)
-  //identify full parseInt
-  fetch(`http://localhost:3000/toys/${e.target.dataset.id}`,{
-    method: "DELETE"
-  })
-    .then(r => {
-      e.target.parentElement.remove()
-    })
-
-  // })//end fetch
 
 
-  }//end of if
 
-})//end of toy container event listener
 
-})//end DOMContentLoaded
+})//end dom DOMContentLoaded
